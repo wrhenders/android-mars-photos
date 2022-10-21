4049907167bdbbe7af345af0b07dbcb49a797ed4
Ryan Henderson <wrhenders@gmail.com>
2022-10-21 10:59:55 -0500

    interface for retrofit GET calls

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.marsphotos.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
@@ -11,8 +12,15 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
-interface MarsApiService
+interface MarsApiService{
+    @GET("photos")
+    suspend fun getPhotos(): String
+}
 
 
-object MarsApiService {
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+
 }
